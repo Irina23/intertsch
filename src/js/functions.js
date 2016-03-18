52ded7a707c037aead51f7a263ba552179393c90
HomePC <ira.intertech@gmail.com>
2016-03-18 19:24:56 +0200

    slider, about, works

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,6 +1,26 @@
 jQuery(document).ready(function() {
 
+    jQuery('.slider_about').bxSlider({
+        auto: true,
+        controls: false
 
+    });
+    jQuery('.slider_main').bxSlider({
+        //auto: true,
+        controls: false
+
+    });
+
+
+
+    jQuery('.slider_works').bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 300,
+        nextText: "",
+        prevText: "",
+        slideMargin: 10
+    });
 
 
 });
